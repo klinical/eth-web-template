8fe191b5848b83ca0d0f53e57acb0aae32bed8a6
zach <zach.n500@gmail.com>
2021-11-10 20:20:51 -0600

    some basic metamask functionality

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,66 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Rabl, powered by Ethereum"/>
+    <div v-if="isMetaMaskInstalled">
+      <div v-if="userLinkedWallet">
+        <Board />
+      </div>
+      <div v-else>
+        <p>A linked wallet is required to use this site. This may change in the future.</p>
+      </div>
+    </div>
+    <div v-else>
+      <p style="text-align: center;">
+        MetaMask is and required to get the full experience of this site (this may change in the future). You may install it for your browser here: 
+      </p>
+      <a href="https://metamask.io/">https://metamask.io/</a>
+    </div>
   </div>
 </template>
 
 <script>
 import Web3 from 'web3'
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 import detectEthereumProvider from '@metamask/detect-provider'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board
+  },
+  data() {
+    return {
+      loading: true,
+      isMetaMaskInstalled: false,
+      userLinkedWallet: false,
+    }
   },
   async mounted() {
-    const provider = await detectEthereumProvider();
+    const { ethereum } = window
+    this.isMetaMaskInstalled = Boolean(ethereum && ethereum.isMetaMask)
 
-    if (provider) {
-      const web3 = new Web3(provider)
+    if (this.isMetaMaskInstalled) {
+      const provider = await detectEthereumProvider();
 
-      this.$store.commit('set_address', web3)
+      if (provider) {
+        const web3 = new Web3(provider)
 
-      try {
-        await provider.request({ method: "eth_requestAccounts" })
-      } catch ( e ) {
-        console.log(e)
+        try {
+          const accounts = await provider.request({ method: "eth_requestAccounts" })
+          console.log(accounts)
+
+          this.$store.commit('set_address', web3)
+          this.$store.commit('set_accounts', accounts)
+
+          this.userLinkedWallet = true
+        } catch ( e ) {
+          console.log(e)
+        }
+      } else {
+        console.log("MetaMask and a valid Ethereum wallet are required to use this website.")
       }
-    } else {
-      console.log("MetaMask and a valid Ethereum wallet are required to use this website.")
     }
+
+    this.loading = false
   }
 }
 </script>
